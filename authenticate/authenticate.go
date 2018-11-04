b626e2aec0479b44a2bec813ae174c4334823edc
Will JV Smith <smithwjv@gmail.com>
2018-11-04 15:02:44 +0000

    Add basic mux server to each service

--- authenticate/authenticate.go
+++ authenticate/authenticate.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Authenticate!\n"))
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("hello authenticate")
+	r := mux.NewRouter()
+	r.HandleFunc("/", TestHandler)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- inventory/inventory.go
+++ inventory/inventory.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Inventory!\n"))
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("hello inventory")
+	r := mux.NewRouter()
+	r.HandleFunc("/", TestHandler)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
