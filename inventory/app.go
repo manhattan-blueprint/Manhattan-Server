7796d81415db447b7c73615e59adad0c4463d5d2
Will JV Smith <smithwjv@gmail.com>
2018-12-31 17:38:13 +0000

    Add authorization header token handling

--- inventory/app.go
+++ inventory/app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -15,6 +16,12 @@ type App struct {
 	Router *mux.Router
 	DB     *sql.DB
 }
+
+type ID struct {
+	Value uint32
+}
+
+const BEARER_PREFIX string = "Bearer "
 
 /* Initialise database connection, mux router and routes */
 func (a *App) Initialise(dbUser, dbPassword, dbHost, dbName string) error {
@@ -59,8 +66,52 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(response)
 }
 
+func checkTokenAndGetID(db *sql.DB, w http.ResponseWriter,
+	r *http.Request) (uint32, bool) {
+	var id ID
+
+	// Get raw Authorization header
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		respondWithError(w, http.StatusBadRequest,
+			"Authorization header required")
+		return id.Value, false
+	}
+
+	// Check request is sending a bearer token
+	if !strings.HasPrefix(authHeader, BEARER_PREFIX) {
+		respondWithError(w, http.StatusBadRequest,
+			"Bearer token required")
+		return id.Value, false
+	}
+
+	// Get token string
+	tokString := authHeader[len(BEARER_PREFIX):]
+
+	// Check token and get user_id
+	stmt := "SELECT user_id FROM token WHERE access=?"
+	err := db.QueryRow(stmt, tokString).Scan(&id.Value)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusUnauthorized,
+				"The access token provided does not match any user")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return id.Value, false
+	}
+	return id.Value, true
+}
+
 /* Validate auth token and return user inventory */
 func (a *App) getInventory(w http.ResponseWriter, r *http.Request) {
+	id, valid := checkTokenAndGetID(a.DB, w, r)
+	if !valid {
+		return
+	}
+	fmt.Printf("ID: %d\n", id)
+
 	respondWithError(w, http.StatusNotImplemented, "To be implemented")
 }
 
