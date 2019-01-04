b8a4450d3470fdfc6bf818cb69ca5a99dd31ea4e
Will JV Smith <smithwjv@gmail.com>
2019-01-04 14:02:18 +0000

    Add add inventory API call

--- inventory/app.go
+++ inventory/app.go
@@ -67,6 +67,15 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(response)
 }
 
+/* Respond with an empty JSON */
+func respondWithEmptyJSON(w http.ResponseWriter, code int) {
+	response := []byte("{}")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+/* Validate auth token and get user ID */
 func getIDFromToken(db *sql.DB, w http.ResponseWriter,
 	r *http.Request) (uint32, error) {
 	var id ID
@@ -94,7 +103,7 @@ func getIDFromToken(db *sql.DB, w http.ResponseWriter,
 			return id.Value,
 				errors.New("The access token provided does not match any user")
 		default:
-			/* TODO: This is really an internal server error, implement a custom
+			/* TODO: This is really an internal server error; implement a custom
 			** error type to return the correct HTTP code to respond to the user
 			 */
 			return id.Value, err
@@ -103,7 +112,23 @@ func getIDFromToken(db *sql.DB, w http.ResponseWriter,
 	return id.Value, nil
 }
 
-/* Validate auth token and return user inventory */
+/* Check sent item list is valid */
+func checkValidInventory(inv Inventory) error {
+	if len(inv.Items) <= 0 {
+		return errors.New("Empty item list")
+	}
+	for i := 0; i < len(inv.Items); i++ {
+		if inv.Items[i].ItemID <= 0 || inv.Items[i].ItemID > 16 {
+			return errors.New("Invalid item ID in list")
+		}
+		if inv.Items[i].Quantity <= 0 {
+			return errors.New("Invalid item quantity in list")
+		}
+	}
+	return nil
+}
+
+/* Return user inventory */
 func (a *App) getInventory(w http.ResponseWriter, r *http.Request) {
 	id, err := getIDFromToken(a.DB, w, r)
 	if err != nil {
@@ -112,15 +137,49 @@ func (a *App) getInventory(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Printf("ID: %d\n", id)
 
+	// stmt := "SELECT item_id, quantity FROM inventory WHERE user_id=?"
+
 	respondWithError(w, http.StatusNotImplemented, "To be implemented")
 }
 
-/* Validate auth token and add item to user inventory */
+/* Add item(s) to user inventory */
 func (a *App) addInventory(w http.ResponseWriter, r *http.Request) {
-	respondWithError(w, http.StatusNotImplemented, "To be implemented")
+	id, err := getIDFromToken(a.DB, w, r)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// Decode json body into inventory struct
+	decoder := json.NewDecoder(r.Body)
+	var inv Inventory
+	err = decoder.Decode(&inv)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid item list")
+		return
+	}
+
+	err = checkValidInventory(inv)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Add user ID to item(s)
+	for i := 0; i < len(inv.Items); i++ {
+		inv.Items[i].UserID = id
+	}
+
+	// Query database
+	err = inv.AddInventory(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	respondWithEmptyJSON(w, http.StatusOK)
 }
 
-/* Validate auth token and remove item from user inventory */
+/* Remove item(s) from user inventory */
 func (a *App) removeInventory(w http.ResponseWriter, r *http.Request) {
 	respondWithError(w, http.StatusNotImplemented, "To be implemented")
 }
